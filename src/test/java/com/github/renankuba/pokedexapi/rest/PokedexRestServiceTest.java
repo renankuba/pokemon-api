62a6ff9b7605999d14d9bc913ed061f71e694d2c
renankuba <kuba.renan@gmail.com>
2022-09-30 16:42:33 +0100

    Clean up rest service unit test

--- src/test/java/com/github/renankuba/pokedexapi/rest/PokedexRestServiceTest.java
+++ src/test/java/com/github/renankuba/pokedexapi/rest/PokedexRestServiceTest.java
@@ -9,16 +9,15 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.github.renankuba.pokedexapi.model.Pokemon;
 import com.github.renankuba.pokedexapi.service.PokemonService;
-@SpringBootTest
-@AutoConfigureMockMvc
+
+@WebMvcTest(PokemonRestService.class)
 public class PokedexRestServiceTest {
     private static final String BASE_URL = "/pokedex/pokemons";
 
@@ -34,6 +33,7 @@ public class PokedexRestServiceTest {
         mockMvc.perform(get(BASE_URL + "/1")
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$.number").isNumber())
             .andExpect(jsonPath("$.number").value(1))
             .andExpect(jsonPath("$.name").value("Bulbassaur"))
@@ -59,6 +59,7 @@ public class PokedexRestServiceTest {
         mockMvc.perform(get(BASE_URL)
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$").isArray())
             .andExpect(jsonPath("$").isNotEmpty())
             .andExpect(jsonPath("$.[0].number").isNumber())
