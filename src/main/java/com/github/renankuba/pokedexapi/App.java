80a865c651c2d6b76b5f5ff3cf2f715691d8404e
renankuba <kuba.renan@gmail.com>
2022-10-01 19:53:33 +0100

    clean up App class

--- src/main/java/com/github/renankuba/pokedexapi/App.java
+++ src/main/java/com/github/renankuba/pokedexapi/App.java
@@ -1,19 +1,10 @@
 package com.github.renankuba.pokedexapi;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 @SpringBootApplication
 public class App {
-    private static Logger logger = LogManager.getLogger(App.class);
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
     }
